9d94254b857100a4a918256be366342783816916
gloriasharmila <37238391+gloriasharmila@users.noreply.github.com>
2019-02-05 19:44:34 +0530

    Update leapyear.py

--- Beginner Level/leapyear.py
+++ Beginner Level/leapyear.py
@@ -1,9 +1,9 @@
 a=int(input())
 if(a%400==0):
-    print ("leapyear")
+    print("leapyear")
 elif(a%4==0):
-    print ("leapyear")
+    print("leapyear")
 elif(a%100!=0):
-    print ("leapyear")
+    print("leapyear")
 else:
-    print ("not leapyear")
+    print("not leapyear")
